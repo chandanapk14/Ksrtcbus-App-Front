cf3a285b4c9857b32a4e69e1e7a854b1e5ebbfb3
chandanapk14 <chandanapushkaran@gmail.com>
2024-06-14 09:39:03 +0530

    Success

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Registeruser from './components/Registeruser';
 import Login from './components/Login';
 import Addbus from './components/Addbus';
 import ViewBus from './components/ViewBus';
+import Search from './components/Search';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Route path='/reg' element={<Registeruser/>}/>
       <Route path='/add' element={<Addbus/>}/>
       <Route path='/viewbus' element={<ViewBus/>}/>
+      <Route path='/search' element={<Search/>}/>
       
       </Routes></BrowserRouter>
   );
---
+++ src/components/Search.jsx
@@ -0,0 +1,99 @@
+import axios from 'axios'
+import React, { useState } from 'react'
+
+const Search = () =>{
+    const [data, setData] = useState(
+
+        {
+            "bname": ""
+
+        }
+
+    )
+    const [result, setResult] = useState([])
+
+
+    const inputHandler = (event) => {
+        setData({ ...data, [event.target.name]: event.target.value })
+    }
+
+    const readValue = () => {
+        console.log(data)
+        axios.post("http://localhost:8061/search",data).then(
+          (response)=>{
+            setResult(response.data)
+          }
+        ).catch().finally()
+ 
+    }
+
+    const deletebus=(id)=>{
+        let input={"_id":id}
+        axios.post("http://localhost:8061/delete",input).then(
+            (response)=>{
+                console.log(response.data)
+                if (response.data.status=="success") {
+                    alert("Successfully Deleted")
+                } else {
+                    alert("Error")
+                }
+
+            }
+        ).catch().finally()
+    }
+    return (
+        <div>
+            <br></br><h3><u><center>Search Bus</center></u></h3><br></br>
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12-col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <div className="row g-3">
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <label htmlFor="" className="form-label">Bus Name</label>
+                                <input type="text" className="form-control" name='bname' value={data.bname} onChange={inputHandler} />
+                            </div>
+                            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                <center><button className="btn btn-success" onClick={readValue}>Search</button></center>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div><br></br>
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <table class="table table-bordered border-primary">
+                        
+                            <thead>
+                                <tr>
+                                    <th scope="col">Bus Name</th>
+                                    <th scope="col">Route</th>
+                                    <th scope="col">Bus No</th>
+                                    <th scope="col">Driver Name</th>
+                                    
+                                    
+                                </tr>
+                            </thead>
+                            <tbody>
+                               {result.map(
+                                (value,index)=>{
+                                    return <tr>
+                                    <td>{value.bname}</td>
+                                    <td>{value.route}</td>
+                                    <td>{value.busno}</td>
+                                    <td>{value.drname}</td>
+                                   
+                                    <td><button className="btn btn-danger" onClick={()=>{deletebus(value._id)}}>Delete</button></td>
+                                </tr>
+                                }
+                               )}
+                                
+                            </tbody>
+                        </table>
+                   
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Search
